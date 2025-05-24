784edbe21c18d56e778a39f399a5770d8dcbdbbb
amaresh329 <vutukuriamaresh930@gmail.com>
2025-05-24 13:53:36 +0530

    Individual Products Page Done

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export {Header} from '../components/Layouts/Header';
-export {Footer} from '../components/Layouts/Footer'
+export {Footer} from '../components/Layouts/Footer';
+export {ProductCard} from '../components/Elements/ProductCard';
+export {Rating} from '../components/Elements/Rating'
